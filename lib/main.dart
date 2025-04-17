a1b05af25922fe456f39d63c70ad84e28a632560
Youssef Shawky <youssefshawky691@gmail.com>
2025-04-17 17:26:42 +0200

    Format MaterialApp widget for improved readability and consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'screens/home_page.dart';
 
 void main() {
@@ -10,6 +11,9 @@ class OneVoice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
